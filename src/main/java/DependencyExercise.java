ad16702a423478bccf449d366397a0ae95cc440e
leec89 <leec89@gmail.com>
2022-06-27 10:43:59 -0500

    feat: first commit maven exercise

---
+++ src/main/java/DependencyExercise.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DependencyExercise {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String someThing = scanner.next();
+        System.out.println("You entered " + someThing);
+        System.out.println();
+
+
+
+    }
+}
