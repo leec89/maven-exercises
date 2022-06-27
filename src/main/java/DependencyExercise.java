dc5a1afb033284e41315bf183a32cae5c908b970
leec89 <leec89@gmail.com>
2022-06-27 11:30:20 -0500

    feat: completed maven exercise

--- src/main/java/DependencyExercise.java
+++ src/main/java/DependencyExercise.java
@@ -1,16 +1,47 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class DependencyExercise {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter something: ");
-        String someThing = scanner.next();
-        System.out.println("You entered " + someThing);
-        System.out.println();
+        // setup do-while loop
+        boolean again = true;
+        do {
 
+            // User input of something
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter something: ");
+            String someThing = scanner.next();
+            System.out.println();
 
+            // Checks to see in the input (someThing) is a number
+            if (StringUtils.isNumeric(someThing)) {
+                System.out.println("You entered " + someThing + " and it is a number.");
+                System.out.println();
+            } else {
+                System.out.println("You entered " + someThing + " and it is NOT a number.");
+                System.out.println();
+
+                // Flips the case of the input string using the SwapCase method
+                System.out.println("The flipped case of the entered string is " + StringUtils.swapCase(someThing) + ".");
+                System.out.println();
+
+                // Reverses the input string using the reverse method
+                System.out.println("The reverse of the entered string is " + StringUtils.reverse(someThing) + ".");
+                System.out.println();
+
+            }
+            System.out.print("Do you want to go again(Y/n)?: ");
+            String askUserToGoAgain = scanner.next().toLowerCase();
+            System.out.println();
+            if (!askUserToGoAgain.equals("y")) {
+                again = false;
+                System.out.println("Thanks for your time.");
+            }
+        } while (again);
 
     }
+
 }
